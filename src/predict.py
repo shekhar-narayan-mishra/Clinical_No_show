72cb92a0180a2b8916bf75fbd610f9d162a1930d
Ayush Kumar <ayushkumarkvs160@gmail.com>
2026-02-24 01:03:33 +0530

    Add predict function to make predictions using model

--- src/predict.py
+++ src/predict.py
@@ -2,3 +2,10 @@ import joblib
 from src.preprocessing import preprocess
 
 model = joblib.load("models/noshow_model.pkl")
+
+def predict(df):
+
+    X = preprocess(df)
+    probs = model.predict_proba(X)[:,1]
+
+    return probs
